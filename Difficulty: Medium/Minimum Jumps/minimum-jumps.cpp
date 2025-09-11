27ede20953179d7ae80a0747b60644ce990b7553
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2025-09-11 06:32:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -0,0 +1,24 @@
+// Function to return minimum number of jumps to end of array
+
+class Solution {
+  public:
+    int minJumps(vector<int>&a) {
+        // Your code here
+        int n = a.size();
+        if(a[0] == 0)
+            return -1;
+            
+        int jumps = 0, mxreachable = 0, limit = 0;
+        for(int i = 0; i < n - 1; i++){
+            mxreachable = max(mxreachable, i + a[i]);
+            if(i == limit){
+                limit = mxreachable;
+                jumps++;
+            }
+        }
+        if(limit < n - 1)
+            return -1;
+        return jumps;
+    }
+};
+
